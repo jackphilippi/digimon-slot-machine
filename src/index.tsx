a0b0034aca34fe864fc908a46cc75c0b0c477494
Jack Philippi <jackphilippi@gmail.com>
2024-01-04 04:12:51 +1100

    Updated package.lock & fixed a couple of bugs

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ ReactDOM.render(
       <h1 className='display-4'>Digimon World 1 Evolution Slot Machine</h1>
       <p className='lead'>This tool provides the criteria for digivolving your digimon into another randomly selected digimon.</p>
       <DigiSlotMachine />
-      <p className='text-end'>If you encounter any problems or would like to request a feature, please raise an issue on Github: <a href="https://github.com/jackphilippi/digislots">https://github.com/jackphilippi/digislots</a>.</p>
+      <p className='text-end'>If you encounter any problems or would like to request a feature, please <a href="https://github.com/jackphilippi/digislots">raise an issue on Github</a>.</p>
     </div>
   </React.StrictMode>,
   document.getElementById('root')
